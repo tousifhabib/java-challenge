c1e05db3e5ba2b4ad1ae4b967f7aaa6c6999b25f
Tousif Habib <thabib@mun.ca>
2023-05-14 17:31:15 -0230

    feat(AuthenticationController): Add AuthenticationController for user authentication
    This commit introduces the AuthenticationController class, responsible for handling user authentication requests. It utilizes the AuthenticationManager, CustomUserDetailsService, and JwtUtil to authenticate users and generate JWT tokens for successful authentication.

---
+++ src/main/java/jp/co/axa/apidemo/controllers/AuthenticationController.java
@@ -0,0 +1,39 @@
+package jp.co.axa.apidemo.controllers;
+
+import jp.co.axa.apidemo.dto.AuthenticationRequest;
+import jp.co.axa.apidemo.dto.AuthenticationResponse;
+import jp.co.axa.apidemo.services.CustomUserDetailsService;
+import jp.co.axa.apidemo.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        } catch (Exception e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        final String jwt = jwtUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+}
