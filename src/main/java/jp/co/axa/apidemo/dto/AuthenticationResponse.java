752b9c9cdb7e558379c478c43a9c8939be96b6ba
Tousif Habib <thabib@mun.ca>
2023-05-14 17:29:16 -0230

    feat(AuthenticationResponse): Add AuthenticationResponse DTO class

---
+++ src/main/java/jp/co/axa/apidemo/dto/AuthenticationResponse.java
@@ -0,0 +1,19 @@
+package jp.co.axa.apidemo.dto;
+public class AuthenticationResponse {
+    private String jwt;
+
+    public AuthenticationResponse() {
+    }
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+}
