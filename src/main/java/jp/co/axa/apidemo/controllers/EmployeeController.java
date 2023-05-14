2f283d1719bf46e66144434a2cfd9dbbfeb377d9
Tousif Habib <thabib@mun.ca>
2023-05-14 00:07:13 -0230

    refactor(EmployeeController): Updated JavaDocs and return types

--- src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
+++ src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 /**
- * The EmployeeController class handles HTTP requests related to employee data.
+ * Controller for managing employees.
  */
 @RestController
 @RequestMapping("/api/v1")
@@ -28,7 +28,7 @@ public class EmployeeController {
     /**
      * Constructs an EmployeeController with the given EmployeeService.
      *
-     * @param employeeService the EmployeeService to be used for employee operations
+     * @param employeeService the service to be used for employee operations
      */
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
@@ -37,26 +37,28 @@ public class EmployeeController {
     /**
      * Retrieves a list of all employees.
      *
-     * @return the list of employees
+     * @return ResponseEntity containing a list of all employees
      */
     @GetMapping("/employees")
-    public List<Employee> getEmployees() {
+    public ResponseEntity<List<Employee>> getEmployees() {
         LOG.info("Retrieving all employees");
-        return employeeService.retrieveEmployees();
+        List<Employee> employees = employeeService.retrieveEmployees();
+        return ResponseEntity.ok(employees);
     }
 
     /**
      * Retrieves an employee by their ID.
      *
      * @param employeeId the ID of the employee to retrieve
-     * @return the employee with the specified ID
+     * @return ResponseEntity containing the employee with the specified ID
      * @throws ResourceNotFoundException if the employee with the given ID is not found
      */
     @GetMapping("/employees/{employeeId}")
-    public Employee getEmployee(@PathVariable(name = "employeeId") @ValidEmployeeId Long employeeId) {
+    public ResponseEntity<Employee> getEmployee(@PathVariable(name = "employeeId") @ValidEmployeeId Long employeeId) {
         LOG.info("Retrieving employee with ID: {}", employeeId);
         try {
-            return employeeService.getEmployee(employeeId);
+            Employee employee = employeeService.getEmployee(employeeId);
+            return ResponseEntity.ok(employee);
         } catch (EmployeeNotFoundException e) {
             LOG.error("Employee not found with ID: {}", employeeId);
             throw new ResourceNotFoundException("Employee not found with ID: " + employeeId);
@@ -67,7 +69,7 @@ public class EmployeeController {
      * Saves a new employee.
      *
      * @param employee the employee to be saved
-     * @return ResponseEntity with status and body message
+     * @return ResponseEntity with status 201 (Created) and a success message in the body
      */
     @PostMapping("/employees")
     public ResponseEntity<String> saveEmployee(@Valid @RequestBody Employee employee) {
@@ -81,7 +83,8 @@ public class EmployeeController {
      * Deletes an employee by their ID.
      *
      * @param employeeId the ID of the employee to delete
-     * @return ResponseEntity with status and body message
+     * @return ResponseEntity with status 200 (OK) and a success message in the body
+     * @throws ResourceNotFoundException if the employee with the given ID is not found
      */
     @DeleteMapping("/employees/{employeeId}")
     public ResponseEntity<String> deleteEmployee(@PathVariable(name = "employeeId") @ValidEmployeeId Long employeeId) {
@@ -97,9 +100,18 @@ public class EmployeeController {
         }
     }
 
+    /**
+     * Updates an existing employee.
+     *
+     * @param employee   the employee object with updated details
+     * @param employeeId the ID of the employee to be updated
+     * @return ResponseEntity containing the updated employee
+     * @throws IllegalArgumentException if the provided employeeId does not match the ID of the employee object
+     * @throws ResourceNotFoundException if the employee with the given ID is not found
+     */
     @PutMapping("/employees/{employeeId}")
-    public ResponseEntity<String> updateEmployee(@Valid @RequestBody Employee employee,
-                                                 @PathVariable(name = "employeeId") @ValidEmployeeId Long employeeId) {
+    public ResponseEntity<Employee> updateEmployee(@Valid @RequestBody Employee employee,
+                                                   @PathVariable(name = "employeeId") @ValidEmployeeId Long employeeId) {
         LOG.info("Updating employee with ID: {}", employeeId);
         if (!employeeId.equals(employee.getId())) {
             LOG.error("Mismatched employee IDs in request. Path variable ID: {}, Employee object ID: {}", employeeId, employee.getId());
@@ -108,8 +120,9 @@ public class EmployeeController {
 
         try {
             employeeService.updateEmployee(employee);
+            Employee updatedEmployee = employeeService.getEmployee(employeeId);
             LOG.info("Employee Updated Successfully");
-            return ResponseEntity.ok().body("Employee Updated Successfully");
+            return ResponseEntity.ok(updatedEmployee);
         } catch (EmployeeNotFoundException e) {
             LOG.error("Employee not found with ID: {}", employeeId);
             throw new ResourceNotFoundException("Employee not found with ID: " + employeeId);
