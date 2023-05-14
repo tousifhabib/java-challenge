8fd69f43bddb52f7bd606ce2cea4e8a6757ad625
Tousif Habib <thabib@mun.ca>
2023-05-14 19:24:08 -0230

    refactor(JwtUtil): Added JavaDocs

--- src/main/java/jp/co/axa/apidemo/util/JwtUtil.java
+++ src/main/java/jp/co/axa/apidemo/util/JwtUtil.java
@@ -8,12 +8,6 @@ import org.springframework.stereotype.Service;
 import java.util.Date;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Service;
-
-import java.util.Date;
 import java.util.function.Function;
 
 @Service
@@ -21,6 +15,12 @@ public class JwtUtil {
 
     private String SECRET_KEY = "secret";
 
+    /**
+     * Generates a JWT token for the given user details.
+     *
+     * @param userDetails The user details.
+     * @return The generated JWT token.
+     */
     public String generateToken(UserDetails userDetails) {
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
@@ -30,27 +30,66 @@ public class JwtUtil {
                 .compact();
     }
 
+    /**
+     * Extracts the username from the JWT token.
+     *
+     * @param token The JWT token.
+     * @return The extracted username.
+     */
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
+    /**
+     * Extracts the expiration date from the JWT token.
+     *
+     * @param token The JWT token.
+     * @return The extracted expiration date.
+     */
     public Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
+    /**
+     * Extracts a specific claim from the JWT token using the provided claims resolver.
+     *
+     * @param token          The JWT token.
+     * @param claimsResolver The claims resolver function.
+     * @param <T>            The type of the claim to extract.
+     * @return The extracted claim.
+     */
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
 
+/**
+     * Extracts all claims from the JWT token.
+     *
+     * @param token The JWT token.
+     * @return The extracted claims.
+     */
     private Claims extractAllClaims(String token) {
         return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
     }
 
+    /**
+     * Checks if the JWT token is expired.
+     *
+     * @param token The JWT token to check for expiration.
+     * @return True if the token is expired, false otherwise.
+     */
     private Boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
 
+    /**
+     * Validates the JWT token for the given user details.
+     *
+     * @param token        The JWT token.
+     * @param userDetails The user details.
+     * @return True if the token is valid, false otherwise.
+     */
     public Boolean validateToken(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
