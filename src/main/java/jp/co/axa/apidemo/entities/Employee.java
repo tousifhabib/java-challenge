724b6b2c8e7643aac228215e439cca319c5e37d2
Tousif Habib <thabib@mun.ca>
2023-05-13 22:40:06 -0230

    feat(Employee): add validation

--- src/main/java/jp/co/axa/apidemo/entities/Employee.java
+++ src/main/java/jp/co/axa/apidemo/entities/Employee.java
@@ -9,6 +9,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name="EMPLOYEE")
@@ -22,16 +25,22 @@ public class Employee {
 
     @Getter
     @Setter
+    @NotNull(message = "Name cannot be null")
+    @Size(min = 1, max = 50, message = "Name must be between 1 and 50 characters")
     @Column(name="EMPLOYEE_NAME")
     private String name;
 
     @Getter
     @Setter
+    @NotNull(message = "Salary cannot be null")
+    @Positive(message = "Salary must be positive")
     @Column(name="EMPLOYEE_SALARY")
     private Integer salary;
 
     @Getter
     @Setter
+    @NotNull(message = "Department cannot be null")
+    @Size(min = 1, max = 50, message = "Department must be between 1 and 50 characters")
     @Column(name="DEPARTMENT")
     private String department;
 
