179e1fbad9c19b88003b97256958bcffa2f4387b
Tousif Habib <thabib@mun.ca>
2023-05-14 19:20:11 -0230

    refactor(EmployeeValidator): Added JavaDocs

--- src/main/java/jp/co/axa/apidemo/validators/EmployeeIdValidator.java
+++ src/main/java/jp/co/axa/apidemo/validators/EmployeeIdValidator.java
@@ -4,13 +4,24 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class EmployeeIdValidator implements ConstraintValidator<ValidEmployeeId, Long> {
-
+    /**
+     * Initializes the validator.
+     *
+     * @param constraintAnnotation the constraint annotation.
+     */
     @Override
     public void initialize(ValidEmployeeId constraintAnnotation) {}
 
+
+    /**
+     * Checks if the employee ID is valid.
+     *
+     * @param employeeId the employee ID to validate.
+     * @param context    the validation context.
+     * @return true if the employee ID is valid, false otherwise.
+     */
     @Override
     public boolean isValid(Long employeeId, ConstraintValidatorContext context) {
-        // Here you can add your validation logic, e.g. check if id is not null and positive
         return employeeId != null && employeeId > 0;
     }
 }
