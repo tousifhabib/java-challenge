5f23ae1030b742e7b1ad9ca7c5f1681a047684d5
Tousif Habib <thabib@mun.ca>
2023-05-14 21:07:50 -0230

    fix(SecurityConfig): Fixed error where Swagger UI doesn't show

--- src/main/java/jp/co/axa/apidemo/config/SecurityConfig.java
+++ src/main/java/jp/co/axa/apidemo/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/swagger-resources/**",
                         "/configuration/security",
                         "/swagger-ui.html",
-                        "/webjars/**,",
+                        "/webjars/**",
                         "/h2-console/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
