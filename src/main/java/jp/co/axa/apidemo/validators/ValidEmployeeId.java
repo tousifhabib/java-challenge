3554c6cdfcf0d764e16ac06070b09f60bd9cd773
Tousif Habib <thabib@mun.ca>
2023-05-14 19:21:15 -0230

    refactor(ValidEmployeeId): Added JavaDocs

--- src/main/java/jp/co/axa/apidemo/validators/ValidEmployeeId.java
+++ src/main/java/jp/co/axa/apidemo/validators/ValidEmployeeId.java
@@ -10,9 +10,24 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidEmployeeId {
 
+    /**
+     * Retrieves the error message to be displayed when the employee ID is invalid.
+     *
+     * @return the error message.
+     */
     String message() default "Invalid employee id";
 
+    /**
+     * Retrieves the validation groups for the constraint.
+     *
+     * @return the validation groups.
+     */
     Class<?>[] groups() default {};
 
+    /**
+     * Retrieves the payload for the constraint.
+     *
+     * @return the payload.
+     */
     Class<? extends Payload>[] payload() default {};
 }
