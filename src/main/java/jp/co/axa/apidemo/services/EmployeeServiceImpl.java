7d633752cdbf99386db21afb792c1b5a666bcf4e
Tousif Habib <thabib@mun.ca>
2023-05-13 22:10:47 -0230

    refactor(EmployeeServiceImpl): Update EmployeeServiceImpl and add exception handling
    This commit refactors the EmployeeServiceImpl class by improving the logging mechanism using Apache Log4j. It also adds more descriptive JavaDoc comments for each method and introduces proper exception handling. The code now throws the EmployeeNotFoundException when retrieving, deleting, or updating an employee who doesn't exist. Additionally, the saveEmployee() method now handles exceptions and logs errors when saving an employee. These changes enhance code readability, maintainability, and error handling within the service implementation.

--- src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
+++ src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
@@ -1,42 +1,109 @@
 package jp.co.axa.apidemo.services;
 
 import jp.co.axa.apidemo.entities.Employee;
+import jp.co.axa.apidemo.exceptions.EmployeeNotFoundException;
 import jp.co.axa.apidemo.repositories.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Objects;
 
+/**
+ * Implementation of the EmployeeService interface.
+ * This service is responsible for the CRUD operations for employees.
+ */
 @Service
-public class EmployeeServiceImpl implements EmployeeService{
+public class EmployeeServiceImpl implements EmployeeService {
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
+    private static final Logger LOGGER = LogManager.getLogger(EmployeeServiceImpl.class);
 
-    public void setEmployeeRepository(EmployeeRepository employeeRepository) {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
     }
 
+    /**
+     * Retrieve all employees.
+     *
+     * @return the list of all employees
+     */
+    @Override
     public List<Employee> retrieveEmployees() {
-        List<Employee> employees = employeeRepository.findAll();
-        return employees;
+        LOGGER.info("Retrieving all employees");
+        return employeeRepository.findAll();
     }
 
+    /**
+     * Retrieve an employee by their id.
+     *
+     * @param employeeId the id of the employee
+     * @return the employee if found
+     * @throws EmployeeNotFoundException if the employee does not exist
+     */
+    @Override
     public Employee getEmployee(Long employeeId) {
-        Optional<Employee> optEmp = employeeRepository.findById(employeeId);
-        return optEmp.get();
+        Objects.requireNonNull(employeeId, "Employee id must not be null");
+        LOGGER.info("Retrieving employee with id: " + employeeId);
+        return employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee with id " + employeeId + " was not found"));
     }
 
-    public void saveEmployee(Employee employee){
-        employeeRepository.save(employee);
+    /**
+     * Save an employee.
+     *
+     * @param employee the employee to save
+     */
+    @Override
+    @Transactional
+    public void saveEmployee(Employee employee) {
+        try {
+            LOGGER.info("Saving employee: " + employee);
+            employeeRepository.save(employee);
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while saving employee: ", e);
+            throw e;
+        }
     }
 
-    public void deleteEmployee(Long employeeId){
-        employeeRepository.deleteById(employeeId);
+    /**
+     * Delete an employee by their id.
+     *
+     * @param employeeId the id of the employee
+     * @throws EmployeeNotFoundException if the employee does not exist
+     */
+    @Override
+    @Transactional
+    public void deleteEmployee(Long employeeId) {
+        Objects.requireNonNull(employeeId, "Employee id must not be null");
+        LOGGER.info("Deleting employee with id: " + employeeId);
+        if (employeeRepository.existsById(employeeId)) {
+            employeeRepository.deleteById(employeeId);
+        } else {
+            throw new EmployeeNotFoundException("Employee with id " + employeeId + " was not found");
+        }
     }
 
+    /**
+     * Update an existing employee.
+     *
+     * @param employee the employee with updated details
+     * @throws EmployeeNotFoundException if the employee does not exist
+     */
+    @Override
+    @Transactional
     public void updateEmployee(Employee employee) {
-        employeeRepository.save(employee);
+        Objects.requireNonNull(employee, "Employee must not be null");
+        Objects.requireNonNull(employee.getId(), "Employee id must not be null");
+        LOGGER.info("Updating employee: " + employee);
+        if (employeeRepository.existsById(employee.getId())) {
+            employeeRepository.save(employee);
+        } else {
+            throw new EmployeeNotFoundException("Employee with id " + employee.getId() + " was not found");
+        }
+
     }
 }
