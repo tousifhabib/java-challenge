813685702ecfaef33c0de42535aa48b04aa55f73
Tousif Habib <thabib@mun.ca>
2023-05-13 22:08:48 -0230

    refactor(EmployeeService): Update EmployeeService interface and add exception handling
    This commit refactors the EmployeeService interface by adding a more descriptive JavaDoc for each method. It also introduces an EmployeeNotFoundException to be thrown when attempting to retrieve an employee who doesn't exist. This update enhances code readability and improves error handling within the service.

--- src/main/java/jp/co/axa/apidemo/services/EmployeeService.java
+++ src/main/java/jp/co/axa/apidemo/services/EmployeeService.java
@@ -1,18 +1,49 @@
 package jp.co.axa.apidemo.services;
 
 import jp.co.axa.apidemo.entities.Employee;
+import jp.co.axa.apidemo.exceptions.EmployeeNotFoundException;
 
 import java.util.List;
 
+/**
+ * Service for managing employees.
+ */
 public interface EmployeeService {
 
+    /**
+     * Retrieve all employees.
+     *
+     * @return the list of all employees
+     */
     public List<Employee> retrieveEmployees();
 
+    /**
+     * Retrieve an employee by their id.
+     *
+     * @param employeeId the id of the employee
+     * @return the employee if found
+     * @throws EmployeeNotFoundException if the employee does not exist
+     */
     public Employee getEmployee(Long employeeId);
 
+    /**
+     * Save an employee.
+     *
+     * @param employee the employee to save
+     */
     public void saveEmployee(Employee employee);
 
+    /**
+     * Delete an employee by their id.
+     *
+     * @param employeeId the id of the employee
+     */
     public void deleteEmployee(Long employeeId);
 
+    /**
+     * Update an existing employee.
+     *
+     * @param employee the employee with updated details
+     */
     public void updateEmployee(Employee employee);
 }
